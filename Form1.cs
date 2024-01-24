6b69872e55c2f9cf1a2e293a67cd88d622422fe2
anonymous813ke18 <hammy79826@gmail.com>
2024-01-24 12:25:57 +0530

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topic4
+{
+    public partial class Task1 : Form
+    {
+        public Task1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(textBox1.Text), sum = 0;
+            for (int i = 0; i <= num; i++)
+            {
+                sum += i;
+            }
+            MessageBox.Show("The Sum is = " + sum);
+        }
+    }
+}
